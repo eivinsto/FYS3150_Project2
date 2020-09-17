8aaf3df990abb2d68318e694c4f7fac70a09aaf9
Anders Petershagen Åsbø <anderpaa@uio.no>
2020-09-17 11:23:00 +0200

    added function for tridag matrix

--- src/functions.h
+++ src/functions.h
@@ -7,4 +7,7 @@ using namespace arma;
 // diagonalize_armadillo.cpp:
 cx_vec diagonalize_arma(mat A);
 
+// create_tridiagonal_matrix.cpp:
+void tridag_mat(mat& A, double a, double b, double c, int N);
+
 #endif
