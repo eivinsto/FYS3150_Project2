ff8d9f4da51b1616c018ee46f8bfee578ec8ba06
Eivind Støland <eivinsto@student.matnat.uio.no>
2020-09-17 11:20:23 +0200

    Added some comments

--- src/jacobi_method.cpp
+++ src/jacobi_method.cpp
@@ -5,7 +5,13 @@
 
 void jacobi_method(mat& A, mat& R, int N)
 {
-  // Cleaning eigenvector matrix
+  /* Function that takes an NxN matrix A, an NxN matrix R and an int N, and
+  ** uses the jacobi method to calculate the eigenvectors and eigenvalues of A.
+  ** The eigenvectors are stored in R, and the returned matrix A has the
+  ** eigenvalues as its diagonal elements.
+  */
+
+  // Cleaning eigenvector matrix (making sure it is an identity matrix)
   for (int i; i<N; ++i){
     for (int j; i<N; ++i){
       if (i==j){
@@ -17,12 +23,16 @@ void jacobi_method(mat& A, mat& R, int N)
     }
   }
 
-  int k,l;
-  double epsilon = 1.0e-8;
+  int k,l;   // To store the location of the largest element not on the diagonal
+  double epsilon = 1.0e-8;  // Tolerance
   double max_number_iterations = double(N)*double(N)*double(N);
   int iterations = 0;
+
   double max_off_diag = max_offdiag(A, &k, &l, N);
 
+  /* Perform rotations until the tolerance is satisfied, or the max number of
+  ** iterations has been reached.
+  */
   while ( fabs(max_off_diag)>epsilon && double(iterations)<max_number_iterations){
     max_off_diag = max_offdiag(A, &k, &l, N);
     rotate(A, R, k, l, N);
@@ -33,6 +43,10 @@ void jacobi_method(mat& A, mat& R, int N)
 
 double max_offdiag(mat& A, int& k, int& l, int N)
 {
+  /* Function that returns the largest element in A that is not on the diagonal
+  ** and stores its indices in k and l.
+  */
+
   double max = 0.0;
   for (int i; i<N; ++i){
     for (int j; i<N; ++j){
@@ -48,8 +62,12 @@ double max_offdiag(mat& A, int& k, int& l, int N)
 
 void rotate(mat& A, mat& R, int k, int l, int N)
 {
+  /* Function that performs the rotation of A. Modifies A and R.
+  */
+
   double s,c;
 
+  // Find elements of rotation matrix
   if (A[k][l] != 0.0){
     double t,tau;
     tau = (A[l][l]- A[k][k])/(2*A[k][l]);
@@ -68,6 +86,7 @@ void rotate(mat& A, mat& R, int k, int l, int N)
     s = 0.0;
   }
 
+  // Create some variables needed later when overwriting the values stored in A.
   double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
   a_kk = A[k][k];
   a_ll = A[l][l];
