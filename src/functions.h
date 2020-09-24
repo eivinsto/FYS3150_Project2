500df5b382bd2c50ca76ab53748fcf4840ed1be7
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-09-24 11:48:49 +0200

    added qdot_matrix_double_eigval to header

--- src/functions.h
+++ src/functions.h
@@ -9,5 +9,6 @@ void anal_eig(vec& eigval, double a, double d, int N);
 void tridag_mat(mat& A, double a, double d, int N);
 void qdot_matrix(mat& A, double rho_max, int N);
 void qdot_matrix_double(mat& A, double rho_max, double omega_r, int N);
+void qdot_matrix_double_eigval(vec& e, double omega_r, int N);
 
 #endif
